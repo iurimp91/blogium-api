1ab549f59d43ceb3f2afc93fcd2725cd20be53a7
Iuri Magnago <iurimp91@gmail.com>
2021-06-10 15:16:19 -0300

    Implementada funções de deletar e editar posts

--- server.js
+++ server.js
@@ -1,35 +1,27 @@
 import express from "express";
 import cors from "cors";
+import fs from "fs";
 
 const server = express();
 
 server.use(cors());
 server.use(express.json());
 
-let posts = [
-    {
-        id: 1,
-        title: 'Hello World',
-        coverUrl: 'https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png',
-        contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
-        content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
-        commentCount: 2
-    }
-];
+let posts = [];
+let postsId = 1;
 
-const comments = [
-    {
-        id: 1,
-        postId: 1,
-        author: 'João',
-        content: 'Muito bom esse post! Tá de parabéns'
-    }, {
-        id: 2,
-        postId: 1,
-        author: 'Maria',
-        content: 'Como faz pra dar palmas?'
-    }
-];
+if(fs.existsSync("./posts.json")) {
+    posts = JSON.parse(fs.readFileSync('./posts.json'));
+    postsId = posts.length + 1;
+}
+
+let comments = [];
+let commentsId = 1;
+
+if(fs.existsSync("./comments.json")) {
+    comments = JSON.parse(fs.readFileSync('./comments.json'));
+    commentsId = comments.length + 1;
+}
 
 server.get("/posts", (req, res) => {
     res.send(posts);
@@ -50,34 +42,98 @@ server.get("/posts/:id/comments", (req, res) => {
 
 server.post("/posts", (req, res) => {
     const { title, coverUrl, content } = req.body;
+
+    if(!title) {
+        return res.status(400).send("Está faltando o título.");
+    } else if (!coverUrl) {
+        return res.status(400).send("Está faltando a imagem.");
+    } else if(!content) {
+        return res.status(400).send("Está faltando o conteúdo.");
+    }
+
     const newContent = content.replace('<p>', '').replace('</p>', '');
+    const id = postsId++;
     const post = {
-        id: Math.floor(Math.random() * 21), //VER SE FICA ASSIM
+        id,
         title,
         coverUrl,
         contentPreview: newContent,
         content: newContent,
         commentCount: 0
     }
+
     posts.push(post);
-    console.log(posts);
     res.send("Post adicionado com sucesso!");
+
+    fs.writeFileSync("posts.json", JSON.stringify(posts));
 });
 
 server.post(`/posts/:id/comments`, (req, res) => {
     const postId = parseInt(req.params.id);
+    const { author, content } = req.body;
+
+    if(!author) {
+        return res.status(400).send("Está faltando o autor.");
+    } else if (!content) {
+        return res.status(400).send("Está faltando o conteúdo.");
+    }
+
     posts.forEach((post) => {
         if(postId === post.id) {
             post.commentCount++;
         }
     });
 
-    const comment = req.body;
-    comment.id = Math.floor(Math.random() * 21); //VER SE FICA ASSIM
+    const comment = {
+        author,
+        content,
+        postId: postId,
+        id: commentsId++,
+    }
     comments.push(comment);
-    console.log(comments);
 
     res.send("Comentário adicionado com sucesso!");
+
+    fs.writeFileSync("comments.json", JSON.stringify(comments));
+    fs.writeFileSync("posts.json", JSON.stringify(posts));
+});
+
+server.put("/posts/:id", (req, res) => {
+    const postId = parseInt(req.params.id);
+    const { content, title, coverUrl } = req.body;
+
+    posts.forEach(post => {
+        if(post.id === postId) {
+            post.content = content;
+            post.title = title;
+            post.coverUrl = coverUrl;
+        }
+    });    
+
+    res.send("Post alterado com sucesso!");
+
+    fs.writeFileSync("posts.json", JSON.stringify(posts));
+});
+
+server.delete("/posts/:id", (req, res) => {
+    const postId = parseInt(req.params.id);
+
+    posts = posts.filter((post) => {
+        postId !== post.id;
+        console.log(postId);
+        console.log(post.id);
+    });
+
+    console.log(posts);
+
+    comments = comments.filter((comment) => {
+        postId !== comment.postId;
+    });
+
+    res.send("Post deletado com sucesso!");
+
+    fs.writeFileSync("posts.json", JSON.stringify(posts));
+    fs.writeFileSync("comments.json", JSON.stringify(comments));
 });
 
 server.listen(5000);
